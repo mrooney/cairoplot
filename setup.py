25f4e2477c23d0f391c6b8fabc2ae971af612726
Paul Hummer <paul@eventuallyanyway.com>
2009-01-01 21:17:55 -0700

    Added distutils setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+'''Cairoplot installation script.'''
+
+from distutils.core import setup
+import os
+
+setup(
+    name='Cairoplot',
+    version='1.1',
+    description='Cairoplot',
+    url='http://linil.wordpress.com/2008/09/16/cairoplot-11/',
+    license='GNU LGPL 2.1',
+    long_description='''
+        Using Python and PyCairo to develop a module to plot graphics in an
+        easy and intuitive way, creating beautiful results to presentations.
+        ''',
+    )
+
