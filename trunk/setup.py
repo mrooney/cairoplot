7667efff486cf4af8313be94d4ec326ccec1ce43
Paul Hummer <paul@eventuallyanyway.com>
2009-01-01 21:31:40 -0700

    Added py_modules

--- trunk/setup.py
+++ trunk/setup.py
@@ -5,14 +5,15 @@ from distutils.core import setup
 import os
 
 setup(
-    name='Cairoplot',
-    version='1.1',
     description='Cairoplot',
-    url='http://linil.wordpress.com/2008/09/16/cairoplot-11/',
     license='GNU LGPL 2.1',
     long_description='''
         Using Python and PyCairo to develop a module to plot graphics in an
         easy and intuitive way, creating beautiful results to presentations.
         ''',
+    name='Cairoplot',
+    py_modules=['CairoPlot'],
+    url='http://linil.wordpress.com/2008/09/16/cairoplot-11/',
+    version='1.1',
     )
 
