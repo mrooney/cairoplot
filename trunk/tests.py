d33e0138c36284643e8bae665f71fd354395d2b5
Rodrigo Moreira Araujo <rodrigo@scrooge>
2009-02-26 23:00:31 -0300

    tests.py  - fixing but created after module rename.

--- trunk/tests.py
+++ trunk/tests.py
@@ -3,39 +3,39 @@ import math
 import random
 import cairoplot
 
-test_bar_plot = 0
-test_scatter_plot = 0
-test_donut_plot = 0
+test_bar_plot = 1
+test_scatter_plot = 1
+test_donut_plot = 1
 test_dot_line_plot = 1
-test_function_plot = 0
-test_gantt_chart = 0
-test_pie_plot = 0
-test_themes = 0
+test_function_plot = 1
+test_gantt_chart = 1
+test_pie_plot = 1
+test_themes = 1
 
 if test_bar_plot:
     #Passing a dictionary
     data = { 'teste00' : [27], 'teste01' : [10], 'teste02' : [18], 'teste03' : [5], 'teste04' : [1], 'teste05' : [22] }
-    CairoPlot.bar_plot ( 'bar_0_dictionary', data, 400, 300, border = 20, grid = True, rounded_corners = True )
+    cairoplot.bar_plot ( 'bar_0_dictionary', data, 400, 300, border = 20, grid = True, rounded_corners = True )
 
     #Using default, rounded corners and 3D visualization
     data = [ [0, 3, 11], [8, 9, 21], [13, 10, 9], [2, 30, 8] ]
     colors = [ (1,0.2,0), (1,0.7,0), (1,1,0) ]
-    CairoPlot.bar_plot ( 'bar_1_default', data, 400, 300, border = 20, grid = True, rounded_corners = False, colors = "yellow_orange_red" )
-    CairoPlot.bar_plot ( 'bar_2_rounded', data, 400, 300, border = 20, grid = True, rounded_corners = True, colors = colors )
-    CairoPlot.bar_plot ( 'bar_3_3D', data, 400, 300, border = 20, grid = True, three_dimension = True, colors = colors )
+    cairoplot.bar_plot ( 'bar_1_default', data, 400, 300, border = 20, grid = True, rounded_corners = False, colors = "yellow_orange_red" )
+    cairoplot.bar_plot ( 'bar_2_rounded', data, 400, 300, border = 20, grid = True, rounded_corners = True, colors = colors )
+    cairoplot.bar_plot ( 'bar_3_3D', data, 400, 300, border = 20, grid = True, three_dimension = True, colors = colors )
 
     #Mixing groups and columns
     data = [ [1], [2], [3,4], [4], [5], [6], [7], [8], [9], [10] ]
-    CairoPlot.bar_plot ( 'bar_4_group', data, 400, 300, border = 20, grid = True )
+    cairoplot.bar_plot ( 'bar_4_group', data, 400, 300, border = 20, grid = True )
 
     #Using no labels, horizontal and vertical labels
     data = [[3,4], [4,8], [5,3], [9,1]]
     y_labels = [ "line1", "line2", "line3", "line4", "line5", "line6" ]
     x_labels = [ "group1", "group2", "group3", "group4" ]
-    CairoPlot.bar_plot ( 'bar_5_no_labels', data, 600, 200, border = 20, grid = True )
-    CairoPlot.bar_plot ( 'bar_6_x_labels', data, 600, 200, border = 20, grid = True, x_labels = x_labels )
-    CairoPlot.bar_plot ( 'bar_7_y_labels', data, 600, 200, border = 20, grid = True, y_labels = y_labels )
-    CairoPlot.bar_plot ( 'bar_8_hy_labels', data, 600, 200, border = 20, grid = True, x_labels = x_labels, y_labels = y_labels )
+    cairoplot.bar_plot ( 'bar_5_no_labels', data, 600, 200, border = 20, grid = True )
+    cairoplot.bar_plot ( 'bar_6_x_labels', data, 600, 200, border = 20, grid = True, x_labels = x_labels )
+    cairoplot.bar_plot ( 'bar_7_y_labels', data, 600, 200, border = 20, grid = True, y_labels = y_labels )
+    cairoplot.bar_plot ( 'bar_8_hy_labels', data, 600, 200, border = 20, grid = True, x_labels = x_labels, y_labels = y_labels )
 
 if test_donut_plot :
     #Define a new backgrond
@@ -45,49 +45,49 @@ if test_donut_plot :
     
     data = {"john" : 700, "mary" : 100, "philip" : 100 , "suzy" : 50, "yman" : 50}
     #Default plot, gradient and shadow, different background
-    CairoPlot.donut_plot( "donut_1_default", data, 600, 400, inner_radius = 0.3 )
-    CairoPlot.donut_plot( "donut_2_gradient_shadow", data, 600, 400, gradient = True, shadow = True, inner_radius = 0.3 )
-    CairoPlot.donut_plot( "donut_3_background", data, 600, 400, background = background, gradient = True, shadow = True, inner_radius = 0.3 )
+    cairoplot.donut_plot( "donut_1_default", data, 600, 400, inner_radius = 0.3 )
+    cairoplot.donut_plot( "donut_2_gradient_shadow", data, 600, 400, gradient = True, shadow = True, inner_radius = 0.3 )
+    cairoplot.donut_plot( "donut_3_background", data, 600, 400, background = background, gradient = True, shadow = True, inner_radius = 0.3 )
 
 if test_dot_line_plot:
     #Default plot
     data = [ 0, 1, 3.5, 8.5, 9, 0, 10, 10, 2, 1 ]
-    CairoPlot.dot_line_plot( "dot_line_1_default", data, 400, 300, border = 50, axis = True, grid = True,
+    cairoplot.dot_line_plot( "dot_line_1_default", data, 400, 300, border = 50, axis = True, grid = True,
                              x_title = "x axis", y_title = "y axis" )
 
     #Labels
     data = { "john" : [-5, -2, 0, 1, 3], "mary" : [0, 0, 3, 5, 2], "philip" : [-2, -3, -4, 2, 1] }
     x_labels = [ "jan/2008", "feb/2008", "mar/2008", "apr/2008", "may/2008" ]
     y_labels = [ "very low", "low", "medium", "high", "very high" ]
-    CairoPlot.dot_line_plot( "dot_line_2_dictionary_labels", data, 400, 300, x_labels = x_labels, 
+    cairoplot.dot_line_plot( "dot_line_2_dictionary_labels", data, 400, 300, x_labels = x_labels, 
                              y_labels = y_labels, axis = True, grid = True,
                              x_title = "x axis", y_title = "y axis", series_legend=True )
     
     #Series legend
     data = { "john" : [10, 10, 10, 10, 30], "mary" : [0, 0, 3, 5, 15], "philip" : [13, 32, 11, 25, 2] }
     x_labels = [ "jan/2008", "feb/2008", "mar/2008", "apr/2008", "may/2008" ]
-    CairoPlot.dot_line_plot( 'dot_line_3_series_legend', data, 400, 300, x_labels = x_labels, 
+    cairoplot.dot_line_plot( 'dot_line_3_series_legend', data, 400, 300, x_labels = x_labels, 
                              axis = True, grid = True, series_legend = True )
 
 if test_function_plot :
     #Default Plot
     data = lambda x : x**2
-    CairoPlot.function_plot( 'function_1_default', data, 400, 300, grid = True, x_bounds=(-10,10), step = 0.1 )
+    cairoplot.function_plot( 'function_1_default', data, 400, 300, grid = True, x_bounds=(-10,10), step = 0.1 )
     
     #Discrete Plot
     data = lambda x : math.sin(0.1*x)*math.cos(x)
-    CairoPlot.function_plot( 'function_2_discrete', data, 800, 300, discrete = True, dots = True, grid = True, x_bounds=(0,80), 
+    cairoplot.function_plot( 'function_2_discrete', data, 800, 300, discrete = True, dots = True, grid = True, x_bounds=(0,80), 
                              x_title = "t (s)", y_title = "sin(0.1*x)*cos(x)")
 
     #Labels test
     data = lambda x : [1,2,3,4,5][x]
     x_labels = [ "4", "3", "2", "1", "0" ]
-    CairoPlot.function_plot( 'function_3_labels', data, 400, 300, discrete = True, dots = True, grid = True, x_labels = x_labels, x_bounds=(0,4), step = 1 )
+    cairoplot.function_plot( 'function_3_labels', data, 400, 300, discrete = True, dots = True, grid = True, x_labels = x_labels, x_bounds=(0,4), step = 1 )
     
     #Multiple functions
     data = [ lambda x : 1, lambda y : y**2, lambda z : -z**2 ]
     colors = [ (1.0, 0.0, 0.0 ), ( 0.0, 1.0, 0.0 ), ( 0.0, 0.0, 1.0 ) ]
-    CairoPlot.function_plot( 'function_4_multi_functions', data, 400, 300, grid = True, series_colors = colors, step = 0.1 )
+    cairoplot.function_plot( 'function_4_multi_functions', data, 400, 300, grid = True, series_colors = colors, step = 0.1 )
 
 if test_gantt_chart :
     #Default Plot
@@ -95,7 +95,7 @@ if test_gantt_chart :
     x_labels = [ 'teste01', 'teste02', 'teste03', 'teste04']
     y_labels = [ '0001', '0002', '0003', '0004', '0005', '0006', '0007', '0008', '0009', '0010' ]
     colors = [ (1.0, 0.0, 0.0), (1.0, 0.7, 0.0), (1.0, 1.0, 0.0), (0.0, 1.0, 0.0) ]
-    CairoPlot.gantt_chart('gantt_1_default', pieces, 500, 350, x_labels, y_labels, colors)
+    cairoplot.gantt_chart('gantt_1_default', pieces, 500, 350, x_labels, y_labels, colors)
 
 
 if test_pie_plot :
@@ -106,14 +106,14 @@ if test_pie_plot :
 
     #Plot data
     data = {"orcs" : 100, "goblins" : 230, "elves" : 50 , "demons" : 43, "humans" : 332}
-    CairoPlot.pie_plot( "pie_1_default", data, 600, 400 )
-    CairoPlot.pie_plot( "pie_2_gradient_shadow", data, 600, 400, gradient = True, shadow = True )
-    CairoPlot.pie_plot( "pie_3_background", data, 600, 400, background = background, gradient = True, shadow = True )
+    cairoplot.pie_plot( "pie_1_default", data, 600, 400 )
+    cairoplot.pie_plot( "pie_2_gradient_shadow", data, 600, 400, gradient = True, shadow = True )
+    cairoplot.pie_plot( "pie_3_background", data, 600, 400, background = background, gradient = True, shadow = True )
 
 if test_scatter_plot:
     #Default data
     data = [ (-2,10), (0,0), (0,15), (1,5), (2,0), (3,-10), (3,5) ]
-    CairoPlot.scatter_plot ( 'cross_1_default', data = data, width = 500, height = 500, border = 20, axis = True, grid = True )
+    cairoplot.scatter_plot ( 'cross_1_default', data = data, width = 500, height = 500, border = 20, axis = True, grid = True )
     
     data = [(-1, -16, 12), (-12, 17, 11), (-4, 6, 5), (4, -20, 12), (13, -3, 21), (7, 14, 20), (-11, -2, 18), (19, 7, 18), (-10, -19, 15),
             (-17, -2, 6), (-9, 4, 10), (14, 11, 16), (13, -11, 18), (20, 20, 16), (7, -8, 15), (-16, 17, 16), (16, 9, 9), (-3, -13, 25),
@@ -121,7 +121,7 @@ if test_scatter_plot:
             (17, -15, 25), (-2, -8, 5), (5, 20, 20), (18, 20, 23), (-20, -16, 17), (-19, -2, 9), (-11, 19, 18), (17, 16, 12), (-5, -20, 15),
             (-20, -13, 10), (-3, 5, 20), (-1, 13, 17), (-11, -9, 11)]
     colors = [ (0,0,0,0.25), (1,0,0,0.75) ]
-    CairoPlot.scatter_plot ( 'cross_2_variable_radius', data = data, width = 500, height = 500, border = 20, 
+    cairoplot.scatter_plot ( 'cross_2_variable_radius', data = data, width = 500, height = 500, border = 20, 
                              axis = True, discrete = True, dots = 2, grid = True, 
                              x_title = "x axis", y_title = "y axis", circle_colors = colors )
     
@@ -134,17 +134,17 @@ if test_scatter_plot:
     ery = [5*random.random() for x in t]
     data = {"exp" : [t,f], "cos" : [t,g], "sin" : [t,h]}
     series_colors = [ (1,0,0), (0,0,0), (0,0,1) ]
-    CairoPlot.scatter_plot ( 'cross_r_exponential', data = data, errorx = [erx,erx], errory = [ery,ery], width = 800, height = 600, border = 20, 
+    cairoplot.scatter_plot ( 'cross_r_exponential', data = data, errorx = [erx,erx], errory = [ery,ery], width = 800, height = 600, border = 20, 
                              axis = True, discrete = False, dots = 5, grid = True, 
                              x_title = "t", y_title = "f(t) g(t)", series_legend=True, series_colors = series_colors )
     
     
 if test_themes :    
     data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-    CairoPlot.bar_plot ( 'bar_color_themes', data, 400, 300, border = 20, grid = True, colors="rainbow" )
+    cairoplot.bar_plot ( 'bar_color_themes', data, 400, 300, border = 20, grid = True, colors="rainbow" )
     
     data = [ lambda x : 1, lambda y : y**2, lambda z : -z**2 ]
-    CairoPlot.function_plot( 'function_color_themes', data, 400, 300, grid = True, series_colors = ["red", "orange", "yellow"], step = 0.1 )
+    cairoplot.function_plot( 'function_color_themes', data, 400, 300, grid = True, series_colors = ["red", "orange", "yellow"], step = 0.1 )
     
     #Scatter x DotLine
     t = [x*0.1 for x in range(0,40)]
@@ -155,6 +155,6 @@ if test_themes :
     ery = [5*random.random() for x in t]
     data = {"exp" : [t,f], "cos" : [t,g], "sin" : [t,h]}
     series_colors = [ (1,0,0), (0,0,0) ]
-    CairoPlot.scatter_plot ( 'scatter_color_themes', data = data, errorx = [erx,erx], errory = [ery,ery], width = 800, height = 600, border = 20, 
+    cairoplot.scatter_plot ( 'scatter_color_themes', data = data, errorx = [erx,erx], errory = [ery,ery], width = 800, height = 600, border = 20, 
                              axis = True, discrete = False, dots = 5, grid = True, 
                              x_title = "t", y_title = "f(t) g(t)", series_legend=True, series_colors = ["red", "blue", "orange"])
